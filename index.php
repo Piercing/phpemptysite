559a2e218aaf91a163b186ede8ede0ececf36fb1
Pedro Martín Gómez <ligartolabs@hotmail.com>
2017-03-21 13:18:59 +0000

    show values index

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
-  echo "Welcome!";
+  $usuario = $_POST['usuario'];
+  $passw   = $_POST['password'];
+  $mail    = $_POST['correo'];
+  $clave   = $_POST['clave'];
+
+  // Incorporar funciones
+  require_once 'funciones_bd.php';
+  $db = new funciones_BD();
+
+  $enlace = mysqli_connect("eu-cdbr-azure-west-d.cloudapp.net", "bf8fe44cc88880", "9e610bd0", "acsm_764524dac413151");
+
+  if (!$enlace) {
+      echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+      echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+      echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+      exit;
+  }
+
+  echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
+  echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+
+  $result = mysql_query($enlace, "INSERT INTO usuario(login,pass,email) VALUES('$usuario', '$passw', '$mail')");
+  // comprobar si la inserccion es correcta y retornar resultado
+  if ($result) {
+      return true;
+  } else {
+      return false;
+  }
+
+  mysqli_close($enlace);
   
 ?>
